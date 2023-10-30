c7fbfe7a397f0c1846c6695bb5f7392ee85c4fc1
Viacheslav Movchanets <slavikmovchanetc@gmail.com>
2023-10-30 15:49:52 +0200

    ООП ЛР3

---
+++ OOPLR3/Form1.cs
@@ -0,0 +1,76 @@
+using Microsoft.VisualBasic;
+
+namespace OOPLR3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Product product = new Product();
+            MessageBox.Show(product.Info());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Vegetable product = new Vegetable();
+            MessageBox.Show(product.Info());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Fruit product = new Fruit();
+            MessageBox.Show(product.Info());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = Convert.ToString(Interaction.InputBox("¬вед≥ть назву продукта", "¬веденн€", "ќг≥рок"));
+                string color = Convert.ToString(Interaction.InputBox("¬вед≥ть  ол≥р продукта", "¬веденн€", "«елений"));
+                double price = Convert.ToDouble(Interaction.InputBox("¬вед≥ть ц≥ну продукта", "¬веденн€", "20"));
+                int quantity = Convert.ToInt32(Interaction.InputBox("¬вед≥ть к≥льк≥сть продукта", "¬веденн€", "2"));
+                Product product = new Product(name, price, quantity);
+                MessageBox.Show(product.Info());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = Convert.ToString(Interaction.InputBox("¬вед≥ть назву продукта", "¬веденн€", "јпельсин"));
+                string flavor = Convert.ToString(Interaction.InputBox("¬вед≥ть смак продукта", "¬веденн€", "—олодкий"));
+                double price = Convert.ToDouble(Interaction.InputBox("¬вед≥ть ц≥ну продукта", "¬веденн€", "60"));
+                int quantity = Convert.ToInt32(Interaction.InputBox("¬вед≥ть к≥льк≥сть продукта", "¬веденн€", "2"));
+                Product product = new Product(name, price, quantity);
+                MessageBox.Show(product.Info());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($" ≥льк≥сть об'Їкт≥в {Product.Count}");
+        }
+    }
+}
---
+++ OOPLR3/Product.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLR3
+{
+    using System;
+
+    class Product
+    {
+        private static int count = 0;
+        protected int nomer;
+
+        public Product()
+        {
+            count++;
+            nomer = count;
+            Name = "Test";
+            Price = 100;
+            Quantity = 5;
+
+
+            Info();
+        }
+        public static int  Count => count;
+        public Product(string name, double price, int quantity)
+        {
+            count++;
+            nomer = count;
+            Name = name;
+            Price = price;
+            Info();
+            Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+     
+        public virtual string Info()
+        {
+          return   $"Об'єкт  #{nomer} Назва: {Name}, Ціна: {Price}";
+        }
+
+        ~Product()
+        {
+            Console.WriteLine($"Об'єкт Product #{nomer} був знищений.");
+          // count--;
+        }
+    }
+
+    class Vegetable : Product
+    {
+        public Vegetable(string name, double price,int quantity, string color)
+            : base(name, price, quantity)
+        {
+            Color = color;
+        }
+        public Vegetable():base("Test Vegetable", 10, 5)
+        {
+            Color = "test";
+        }
+        public string Color { get; set; }
+        public override string Info()
+        {
+            return "Овоч " + base.Info() + $", Колір {Color}";
+        }
+
+    }
+
+    class Fruit : Product
+    {
+        public Fruit(string name, double price,  int quantity, string flavor)
+            : base(name, price, quantity)
+        {
+            Flavor = flavor;
+        }
+        public Fruit():base("Test Fruit", 20, 3) 
+        {
+            Flavor = "test";
+        }
+        public string Flavor { get; set; }
+
+        public override string Info()
+        {
+            return "Фрукт " + base.Info() + $", Смак {Flavor}";
+        }
+    }
+
+
+
+}
